009d9570584a3fcfef1504cead87032477a9d839
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-06-22 10:47:45 +0000

    Get particular author Books API Added successfully

--- myapp/index.js
+++ myapp/index.js
@@ -150,3 +150,16 @@ app.delete("/books/:bookId", async (request, response) => {
   await db.run(deleteBookQuery);
   response.send("Book Deleted Successfully");
 });
+
+app.get("/authors/:authorId/books", async (request, response) => {
+  const { authorId } = request.params;
+  const getAuthorBooksQuery = `
+    SELECT
+    *
+    FROM
+        book
+    WHERE
+        author_id = ${authorId};`;
+  const booksArray = await db.all(getAuthorBooksQuery);
+  response.send(booksArray);
+});
