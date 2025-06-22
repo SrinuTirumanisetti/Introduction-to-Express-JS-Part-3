737ea48a9ec00b571d7138e741db0e73aec1c23f
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-06-22 09:52:29 +0000

    API to add book is created which is post method

--- myapp/index.js
+++ myapp/index.js
@@ -5,6 +5,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
+app.use(express.json());
 const dbPath = path.join(__dirname, "goodreads.db");
 
 let db = null;
@@ -44,4 +45,58 @@ app.get("/books/:bookId/", async (request, response) => {
   const getBookQuery = `select * from book where book_id=${bookId};`;
   const Book = await db.get(getBookQuery);
   response.send(Book);
+});
+
+//Add  Book APi
+app.use(express.json()); // Ensure body parsing is enabled
+
+app.post("/books", async (request, response) => {
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+
+  const addBookQuery = `
+    INSERT INTO
+      book (
+        title,
+        author_id,
+        rating,
+        rating_count,
+        review_count,
+        description,
+        pages,
+        date_of_publication,
+        edition_language,
+        price,
+        online_stores
+      )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+
+  const dbResponse = await db.run(addBookQuery, [
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  ]);
+
+  const bookId = dbResponse.lastID;
+  response.send({ bookId: bookId });
 });
